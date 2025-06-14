08efc52efaa604447e5ec6f2ed7533d168689045
Sophia Baranova <sonya.brnv@gmail.com>
2025-06-14 20:10:39 +0300

    Move the WeatherData class to a separate file

---
+++ weatherforecast/WeatherData.cs
@@ -0,0 +1,17 @@
+﻿namespace weatherforecast
+{
+    public class WeatherData
+    {
+        public int ID { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Date { get; set; }
+        public double Humidity { get; set; }
+        public string Precipitation { get; set; }
+        public double PrecipitationVal { get; set; }
+        public double Pressure { get; set; }
+        public double Temperature { get; set; }
+        public string Wind { get; set; }
+        public double WindSpeed { get; set; }
+    }
+}
