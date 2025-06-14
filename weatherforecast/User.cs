39a4edb817acae59127bb4f7be86e809e73e8077
Sophia Baranova <sonya.brnv@gmail.com>
2025-06-14 20:09:27 +0300

    Move the User class to a separate file

---
+++ weatherforecast/User.cs
@@ -0,0 +1,11 @@
+﻿namespace weatherforecast
+{
+
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public bool Admin { get; set; }
+    }
+
+}
